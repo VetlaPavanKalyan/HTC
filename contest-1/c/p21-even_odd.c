417931691df6f89e979c2ac9131acc27ead21652
Nireesha7979 <81856769+Nireesha7979@users.noreply.github.com>
2022-05-19 19:42:29 +0530

    Create p21-even_odd.c

---
+++ contest-1/c/p21-even_odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("EVEN");
+    }
+    else{
+        printf("ODD");
+    }
+    return 0;
+}
