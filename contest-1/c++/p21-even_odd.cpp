ac4bca19f6baa2c86b63cf4a7c54cefafc9add95
yaswanth <yaswanth14333@gmail.com>
2022-05-19 10:31:24 +0530

    Create p21-even_odd.cpp

---
+++ contest-1/c++/p21-even_odd.cpp
@@ -0,0 +1,15 @@
+// Write a program to Check Whether a Number is Even or Odd.
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin>>n;
+    if(n%2==0) cout<<"EVEN";
+    else cout<<"ODD";
+    return 0;
+}
