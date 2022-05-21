1b4675ef27379d986e2fabefa690871dc156cce6
anjali-2003 <96301802+anjali-2003@users.noreply.github.com>
2022-05-21 22:57:55 +0530

    Create p40-number_of_vowels.c

---
+++ contest-1/c/p40-number_of_vowels.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    char s[100];
+    int i,c=0;
+    fgets(s,100,stdin);
+    for(i=0;s[i]!='\0';i++){
+        if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')
+            c++;
+    }
+    printf("%d",c);
+    return 0;
+}
